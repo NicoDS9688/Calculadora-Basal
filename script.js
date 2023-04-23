c1e0ece56eac3efc80aa3cdb2deff3c0722569f7
NicoDS9688 <131322379+NicoDS9688@users.noreply.github.com>
2023-04-23 16:55:28 -0400

    Calculadora Basal finalizada

--- script.js
+++ script.js
@@ -5,36 +5,59 @@ const MAN = document.getElementById('man');
 
 CALCULAR.addEventListener('click', () => {
     const DATO = document.getElementById('peso').value
-    
-    if (DATO > 0){
+
+    if (DATO > 0 && DATO <= 30) {
         ERROR.style.display = 'none'
         let flujo = calcFlujo(DATO);
-        let mantenimiento = flujo*1.5;
+        let mantenimiento = flujo * 1.5;
         FLU.innerHTML = flujo + ' cc/hr';
-        MAN.innerHTML = 'm+m/2 ' + mantenimiento + ' cc/hr';
+        MAN.innerHTML = 'm+m/2: ' + mantenimiento.toFixed(2) + ' cc/hr';
         FLU.style.display = 'block';
         MAN.style.display = 'block';
+
+    } else if (DATO > 30) {
+
+        let flujo = calcFlujo(DATO);
+        let flujo1 = (flujo * 1500) / 24;
+        let flujo2 = (flujo * 2000) / 24;
+        let f1 = flujo1.toFixed(2);
+        let f2 = flujo2.toFixed(2);
+        FLU.innerHTML = 'SC1: ' + f1 + ' cc/hr';
+        MAN.innerHTML = 'SC2: ' + f2 + ' cc/hr';
+        FLU.style.display = 'block';
+        MAN.style.display = 'block';
+
     } else {
         ERROR.style.display = 'block';
         FLU.style.display = 'none';
         MAN.style.display = 'none';
     }
+
 })
 
-function calcFlujo(peso){
+function calcFlujo(peso) {
     let resto = peso;
     let flujo = 0;
-    if (resto>20){
-        let aux = resto-20;
-        flujo += aux*1;
-        resto -= aux;
-    } 
-    if (resto>10){
-        let aux = resto-10;
-        flujo += aux*2;
-        resto -= aux;
+    if (resto < 10) {
+        resto = resto * 100;
     }
-    flujo += resto*4;
-    return flujo;
-}
+    else if (resto >= 10 && resto < 20) {
+        let aux;
+        aux = resto - 10;
+        resto = 1000 + (aux * 50);
+    }
 
+    else if (resto >= 20 && resto < 31) {
+        let aux;
+        aux = resto - 20;
+        resto = 1500 + (aux * 20);
+    }
+
+    else if (resto > 30) {
+        sc1 = ((resto * 4) + 7) / (resto + 90);
+        sc2 = resto;
+    }
+
+    flujo = resto / 24;
+    return flujo.toFixed(2);
+}
